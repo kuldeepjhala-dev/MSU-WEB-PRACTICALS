5276ca8d681ea9b19e8430a9e3568324116cbd7e
kuldeepjhala-dev <kuldeephjhala@gmail.com>
2023-11-05 21:19:51 +0530

    final commit

---
+++ LAB-5/3/lab-5_3.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#div1").animate({opacity:'0.4',
+                            width:'500px',
+                            height:'500px'
+        });
+    });
+});
---
+++ LAB-5/4/lab-5_4.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#div1").animate({'font-size':'100px',
+                            width:'5em',
+                            height:'2em'
+    });
+    });
+}); 
+
